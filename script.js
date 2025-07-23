74fd2cbc13136f71c4386cbbc8c2cd19d5a043b3
kolecki4 <55369795+kolecki4@users.noreply.github.com>
2025-07-23 00:08:29 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 
 function setTheme(theme) {
   if (theme == 'poster') {
-    document.getElementById('switcher-id').href = './css/poster.scss';
+    document.getElementById('switcher-id').href = './css/poster.css';
   } else if (theme == 'genealogy') {
-    document.getElementById('switcher-id').href = './css/genealogy.scss';
+    document.getElementById('switcher-id').href = './css/genealogy.css';
   } else if (theme == 'astrowebindex') {
-    document.getElementById('switcher-id').href = './css/astrowebindex.scss';
+    document.getElementById('switcher-id').href = './css/astrowebindex.css';
   } else if (theme == 'exo') {
-    document.getElementById('switcher-id').href = './css/exoplanetarium_mainpage.scss';
+    document.getElementById('switcher-id').href = './css/exoplanetarium_mainpage.css';
   }
   //localStorage.setItem('style', theme);
 }
